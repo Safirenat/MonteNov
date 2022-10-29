9711992825090bef47b71c0b8e230eaa5d1fd244
Your Name <you@example.com>
2022-10-29 15:41:31 +0300

    first commit

---
+++ src/components/BackToTopButton.js
@@ -0,0 +1,56 @@
+// import React from "react";
+import React, { useState, useEffect } from "react";
+
+// import "../../App.scss";
+// import "./Main.scss";
+
+
+export const BackToTopButton = () => {
+
+    const [backToTopButton, setBackToTopButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 300) {
+                setBackToTopButton(true)
+            } else {
+                setBackToTopButton(false)
+
+            }
+        })
+    }, [])
+    
+    
+
+    const scrollUp = () => {
+      try {
+        window.scroll({
+          behavior: 'smooth',
+          top: 0,
+        })
+      } catch(err) {
+        if (err instanceof TypeError) {
+          window.scroll(0, 0)
+        } else {
+          throw err
+        }
+      }
+    }
+
+
+	return (
+		<div>
+            {backToTopButton && (
+                <button style={{
+                    position:"fixed",
+                    bottom: "50px",
+                    right: "50px",
+                    height: "50px",
+                    width: "50px",
+                    fontSize: "50px",
+                }}
+                onClick={scrollUp}>^</button>
+            )}
+        </div>
+	);
+};
