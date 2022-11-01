6ca899c453322099f7d96eef8f7d191932937d55
Your Name <you@example.com>
2022-11-01 22:12:54 +0300

    add-mobile-full-posl-21

--- src/components/Analiz/Analiz.js
+++ src/components/Analiz/Analiz.js
@@ -1,15 +1,91 @@
 import React from "react";
+import {useState} from "react";
 import "./Analiz.scss";
+// import emailjs from '@emailjs/browser';
+import { notification } from 'antd-notifications-messages';
+import 'antd-notifications-messages/lib/styles/style.css';
+import { send } from 'emailjs-com';
 
 
 import { ReactComponent as Arrow } from "../../assets/Images/arrow4.svg";
 
 
 export const Analiz = () => {
+
+	const [toSend, setToSend] = useState({
+		from_name: '',
+		to_name: '',
+		message: '',
+		reply_to: '',
+	  });
+	
+	  const onSubmit = (e) => {
+		e.preventDefault();
+		send(
+		  'service_uf2cf4l',
+		  'template_hs0jgnl',
+		  toSend,
+		  'User ID'
+		)
+		  .then((response) => {
+			console.log('SUCCESS!', response.status, response.text);
+		  })
+		  .catch((err) => {
+			console.log('FAILED...', err);
+		  });
+	  };
+
+	  const show = (type) => {
+		notification({
+		  title: 'Успешно',
+		  message: `Успешно отправлено`
+		});
+	  };
+	
+	  const handleChange = (e) => {
+		setToSend({ ...toSend, [e.target.name]: e.target.value });
+	  };
+
+	  
+	
 	return (
 		<div className="benefits-background">
 			<div className="container">
-				<div className="analiz">
+				<div>
+				<form onSubmit={onSubmit}>
+  <input
+    type='text'
+    name='from_name'
+    placeholder='from name'
+    value={toSend.from_name}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='to_name'
+    placeholder='to name'
+    value={toSend.to_name}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='message'
+    placeholder='Your message'
+    value={toSend.message}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='reply_to'
+    placeholder='Your email'
+    value={toSend.reply_to}
+    onChange={handleChange}
+  />
+  <button onClick={() => show('success')} type='submit'>Submit</button>
+</form>
+
+				</div>
+				{/* <div className="analiz">
 					<p>
 						Получите бесплатный
 						<br /> анализ документов
@@ -24,8 +100,8 @@ export const Analiz = () => {
 								Связаться с нами
 							</a>
 						</div>
-					</div>
-				</div>
+					</div> */}
+				{/* </div> */}
 			</div>
 		</div>
 	);
